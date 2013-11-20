6f74e46369872c485d68153514ee1ee7c358474d
Matus Chochlik <chochlik@gmail.com>
2013-11-20 22:44:43 +0100

    Minor changes in the global namespace registering

--- include/mire/reg/global_scope.hpp
+++ include/mire/reg/global_scope.hpp
@@ -19,6 +19,82 @@ using namespace ::mire::tags;
 template <typename T>
 struct _type_reg;
 
+struct _
+{
+	typedef meta_global_scope_tag category;
+	typedef mire::reg::_ scope;
+	struct base_name
+	{
+		static constexpr const char* c_str = "";
+		static constexpr std::size_t size = 0;
+	};
+	struct full_name
+	{
+		static constexpr const char* c_str = "";
+		static constexpr std::size_t size = 0;
+	};
+
+	template <typename X>
+	struct named_typedef
+	{
+		typedef X _;
+	};
+
+	template <typename X>
+	struct named_mem_var
+	{
+		X _;
+
+		named_mem_var(void) = default;
+
+		template <typename ... P>
+		named_mem_var(P&& ... p)
+		 : _(std::forward<P>(p)...)
+		{ }
+	};
+};
+
+struct anon_t
+{
+	typedef meta_type_tag category;
+	typedef mire::reg::_ scope;
+	struct base_name
+	{
+		static constexpr const char* c_str = "";
+		static constexpr std::size_t size = 0;
+	};
+	struct full_name
+	{
+		static constexpr const char* c_str = "<anon>";
+		static constexpr std::size_t size = 6;
+	};
+
+	template <typename X>
+	struct named_typedef
+	{
+		typedef X _;
+	};
+
+	template <typename X>
+	struct named_mem_var
+	{
+		X _;
+
+		named_mem_var(void) = default;
+
+		template <typename ... P>
+		named_mem_var(P&& ... p)
+		 : _(std::forward<P>(p)...)
+		{ }
+	};
+};
+
+template <>
+struct _type_reg<anon_t>
+{
+	typedef anon_t type;
+};
+
 template <typename T>
 struct _type_reg<T const>
  : _type_reg<T>
@@ -89,40 +165,10 @@ struct _type_reg<T const volatile [N]>
  : _type_reg<T>
 { };
 
-struct _
-{
-	typedef meta_global_scope_tag category;
-	typedef mire::reg::_ scope;
-	struct base_name
-	{
-		static constexpr const char* c_str = "";
-		static constexpr std::size_t size = 0;
-	};
-	struct full_name
-	{
-		static constexpr const char* c_str = "";
-		static constexpr std::size_t size = 0;
-	};
-
-	template <typename X>
-	struct named_typedef
-	{
-		typedef X _;
-	};
-
-	template <typename X>
-	struct named_mem_var
-	{
-		X _;
-
-		named_mem_var(void) = default;
-
-		template <typename ... P>
-		named_mem_var(P&& ... p)
-		 : _(std::forward<P>(p)...)
-		{ }
-	};
-};
+template <typename RV, typename ... P>
+struct _type_reg<RV(P...)>
+ : _type_reg<anon_t>
+{ };
 
 } // namespace reg
 } // namespace mire
