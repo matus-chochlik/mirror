eda1b6985bde75930887f126488767d9e255dbee
Matus Chochlik <chochlik@gmail.com>
2016-08-16 20:31:56 +0200

    Multiple changes
    - Added a fallback implementation for std::basic_string_view.
    - Updated the build system to use the fallback implementation if the
      standard one is missing.
    - Other minor updates in the build system.

---
+++ config/CppIncludes.cmake
@@ -0,0 +1,16 @@
+#  Copyright Matus Chochlik.
+#  Distributed under the Boost Software License, Version 1.0.
+#  See accompanying file LICENSE_1_0.txt or copy at
+#   http://www.boost.org/LICENSE_1_0.txt
+#
+include(CheckIncludeFileCXX)
+check_include_file_cxx("string_view" HAS_STRING_VIEW)
+if (NOT HAS_STRING_VIEW)
+	configure_file(
+		"${PROJECT_SOURCE_DIR}/config/include/string_view"
+		"${PROJECT_BINARY_DIR}/include/string_view" COPY_ONLY
+	)
+endif()
+
+add_definitions(-isystem "${PROJECT_BINARY_DIR}/include")
+
