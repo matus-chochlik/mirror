9114c1f0d437bc716ac3dbb02999cca9f3ce9d51
Matus Chochlik <chochlik@gmail.com>
2016-09-03 10:00:55 +0200

    Updated the quickbook docs

--- doc/quickbook/reflexpr/intro/hello_world.cpp
+++ doc/quickbook/reflexpr/intro/hello_world.cpp
@@ -5,8 +5,29 @@
  *  http://www.boost.org/LICENSE_1_0.txt
  */
 //[reflexpr_example_hello_world_1
-#include <reflexpr> /*<
+#include <__reflexpr__> /*<
 This header implements the metaobject operation templates.
 >*/
+#include <iostream>
+
+namespace Hello {
+
+struct World { };
+
+} // namespace Hello
+
+int main(void)
+{
+	using namespace std;
+
+	using meta_HW = __reflexpr(Hello::World);
+
+	cout << __meta_get_base_name_v<__meta_get_scope_m<meta_HW>>;
+	cout << ", ";
+	cout << __meta_get_base_name_v<meta_HW>;
+	cout << "." << std::endl;
+
+	return 0;
+}
 //]
 
