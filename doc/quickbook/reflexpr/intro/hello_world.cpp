f6f5fd5ef79c731526d7b6433cdf5f0335b66ba9
Matus Chochlik <chochlik@gmail.com>
2016-09-03 08:18:40 +0200

    Added the initial version of quickbook docs

---
+++ doc/quickbook/reflexpr/intro/hello_world.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright Matus Chochlik.
+ * Distributed under the Boost Software License, Version 1.0.
+ * See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt
+ */
+//[reflexpr_example_hello_world_1
+#include <reflexpr> /*<
+This header implements the metaobject operation templates.
+>*/
+//]
+
