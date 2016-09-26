83ffac70666a93e0d4dff9faf759ab57f35034c7
Matus Chochlik <chochlik@gmail.com>
2016-09-26 21:07:28 +0200

    Added a new reflexpr example

---
+++ example/reflexpr/006_enum_to_string.cpp
@@ -0,0 +1,78 @@
+/**
+ * @example reflexpr/006_enum_to_string.cpp
+ * @brief Shows how to implement a enumerator-to-string conversion
+ *
+ * Copyright Matus Chochlik.
+ * Distributed under the Boost Software License, Version 1.0.
+ * See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#include <reflexpr>
+#include <iostream>
+#include <string>
+#include <map>
+
+enum class E
+{
+	a, b, c, d, e
+};
+
+template <typename Enum>
+class enum_to_string
+{
+private:
+	template <typename ... MEC>
+	struct _hlpr
+	{
+		static void _eat(bool ...) { }
+
+		static std::map<Enum, std::string> _make_map(void)
+		{
+			using namespace std;
+
+			map<Enum, string> res;
+			_eat(res.emplace(
+				meta::get_constant_v<MEC>,
+				string(meta::get_base_name<MEC>())
+			).second...);
+			return res;
+		}
+	};
+
+	static auto _make_map(void)
+	{
+		using namespace std;
+
+		using ME = reflexpr(Enum);
+		using hlpr = meta::unpack_sequence_t<
+			meta::get_enumerators_m<ME>,
+			_hlpr
+		>;
+		return hlpr::_make_map();
+	}
+
+	const std::map<Enum, std::string> _map;
+public:
+	enum_to_string(void)
+	 : _map(_make_map())
+	{ }
+
+	const std::string& operator()(Enum e) const
+	{
+		return _map.find(e)->second;
+	}
+};
+
+int main(void)
+{
+	enum_to_string<E> ets;
+
+	std::cout << ets(E::a) << std::endl;
+	std::cout << ets(E::b) << std::endl;
+	std::cout << ets(E::c) << std::endl;
+	std::cout << ets(E::d) << std::endl;
+	std::cout << ets(E::e) << std::endl;
+
+	return 0;
+}
