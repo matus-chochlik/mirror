c3f1350daaa2f04ad8e6a2aa6133e4dba6f85c80
Matus Chochlik <chochlik@gmail.com>
2016-10-17 18:49:05 +0200

    Updated reflexpr tests

--- test/reflexpr/reflects_same_r.cpp
+++ test/reflexpr/reflects_same_r.cpp
@@ -42,8 +42,23 @@ BOOST_AUTO_TEST_CASE(mirror_reflects_same_1)
 	>));
 
 	BOOST_CHECK(( reflects_same_v<
+		reflexpr(bool),
+		reflexpr(bool)
+	>));
+
+	BOOST_CHECK(( reflects_same_v<
 		reflexpr(float),
 		reflexpr(float)
+	>));
+
+	BOOST_CHECK(( reflects_same_v<
+		reflexpr(double),
+		reflexpr(double)
+	>));
+
+	BOOST_CHECK(( reflects_same_v<
+		reflexpr(void),
+		reflexpr(void)
 	>));
 
 	BOOST_CHECK(( reflects_same_v<
@@ -59,6 +74,36 @@ BOOST_AUTO_TEST_CASE(mirror_reflects_same_1)
 	BOOST_CHECK(( reflects_same_v<
 		reflexpr(public),
 		reflexpr(public)
+	>));
+
+	BOOST_CHECK(( reflects_same_v<
+		reflexpr(protected),
+		reflexpr(protected)
+	>));
+
+	BOOST_CHECK(( reflects_same_v<
+		reflexpr(private),
+		reflexpr(private)
+	>));
+
+	BOOST_CHECK(( reflects_same_v<
+		reflexpr(class),
+		reflexpr(class)
+	>));
+
+	BOOST_CHECK(( reflects_same_v<
+		reflexpr(struct),
+		reflexpr(struct)
+	>));
+
+	BOOST_CHECK(( reflects_same_v<
+		reflexpr(union),
+		reflexpr(union)
+	>));
+
+	BOOST_CHECK(( reflects_same_v<
+		reflexpr(enum),
+		reflexpr(enum)
 	>));
 }
 
@@ -82,8 +127,48 @@ BOOST_AUTO_TEST_CASE(mirror_reflects_same_2)
 	>));
 
 	BOOST_CHECK((!reflects_same_v<
+		reflexpr(bool),
+		reflexpr(void)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
 		reflexpr(float),
 		reflexpr(long)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(double),
+		reflexpr(bool)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(float),
+		reflexpr(class)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(double),
+		reflexpr(struct)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(short),
+		reflexpr(union)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(std),
+		reflexpr(static)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(std::string),
+		reflexpr(float)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(std::wstring),
+		reflexpr(unsigned)
 	>));
 
 	BOOST_CHECK((!reflects_same_v<
@@ -92,8 +177,38 @@ BOOST_AUTO_TEST_CASE(mirror_reflects_same_2)
 	>));
 
 	BOOST_CHECK((!reflects_same_v<
+		reflexpr(public),
+		reflexpr(protected)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(public),
+		reflexpr(private)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
 		reflexpr(protected),
 		reflexpr(private)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(class),
+		reflexpr(public)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(struct),
+		reflexpr(protected)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(static),
+		reflexpr(private)
+	>));
+
+	BOOST_CHECK((!reflects_same_v<
+		reflexpr(static),
+		reflexpr(union)
 	>));
 }
 
