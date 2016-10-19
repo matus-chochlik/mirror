88eb5fa832e107c9858f83a4a4e50740cf658132
Matus Chochlik <chochlik@gmail.com>
2016-10-19 21:43:57 +0200

    Updated the quickbook docs

--- doc/quickbook/refltool/quickref/string_to_enum.cpp
+++ doc/quickbook/refltool/quickref/string_to_enum.cpp
@@ -4,12 +4,12 @@
  * See accompanying file LICENSE_1_0.txt or copy at
  *  http://www.boost.org/LICENSE_1_0.txt
  */
-//[refltool_make_enum_to_string_map
+//[refltool_make_string_to_enum_map
 template <typename T /*< Must be an enumeration type >*/>
-constexpr __std_map<T, __std_string> make_enum_to_string_map(void);
+constexpr __std_map<__std_string, T> make_string_to_enum_map(void);
 //]
-//[refltool_enum_to_string
+//[refltool_string_to_enum
 template <typename T /*< Must be an enumeration type >*/>
-const __std_string& enum_to_string(T v);
+T string_to_enum(const __std_string& s);
 //]
 
---
+++ doc/quickbook/refltool/quickref/tie_enumerators.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright Matus Chochlik.
+ * Distributed under the Boost Software License, Version 1.0.
+ * See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt
+ */
+//[refltool_tie_enumerators
+template <typename T>
+constexpr __std_array<T, __unspecified__> tie_enumerators(void);
+//]
+
