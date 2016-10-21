140b701659d8fd2082bf99e8276eb6f9bc879ee5
Matus Chochlik <chochlik@gmail.com>
2016-10-21 06:50:54 +0200

    Updated the quickbook docs

--- doc/quickbook/refltool/quickref/data_member_cmp.cpp
+++ doc/quickbook/refltool/quickref/data_member_cmp.cpp
@@ -36,4 +36,68 @@ struct not_equal_data_members<void>
 
 constexpr const not_equal_data_members<> data_members_not_equal = {};
 //]
+//[refltool_less_data_members
+template <typename T = void>
+struct less_data_members
+{
+	constexpr bool operator()(const T& a, const T& b) const;
+};
+
+template <>
+struct less_data_members<void>
+{
+	template <typename T>
+	constexpr bool operator()(const T& a, const T& b) const;
+};
+
+constexpr const less_data_members<> data_members_less = {};
+//]
+//[refltool_less_equal_data_members
+template <typename T = void>
+struct less_equal_data_members
+{
+	constexpr bool operator()(const T& a, const T& b) const;
+};
+
+template <>
+struct less_equal_data_members<void>
+{
+	template <typename T>
+	constexpr bool operator()(const T& a, const T& b) const;
+};
+
+constexpr const less_equal_data_members<> data_members_less_equal = {};
+//]
+//[refltool_greater_data_members
+template <typename T = void>
+struct greater_data_members
+{
+	constexpr bool operator()(const T& a, const T& b) const;
+};
+
+template <>
+struct greater_data_members<void>
+{
+	template <typename T>
+	constexpr bool operator()(const T& a, const T& b) const;
+};
+
+constexpr const greater_data_members<> data_members_greater = {};
+//]
+//[refltool_greater_equal_data_members
+template <typename T = void>
+struct greater_equal_data_members
+{
+	constexpr bool operator()(const T& a, const T& b) const;
+};
+
+template <>
+struct greater_equal_data_members<void>
+{
+	template <typename T>
+	constexpr bool operator()(const T& a, const T& b) const;
+};
+
+constexpr const greater_equal_data_members<> data_members_greater_equal = {};
+//]
 
