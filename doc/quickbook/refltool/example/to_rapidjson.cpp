0aba685d4f792941f2d87e8bf1ccb61a233951e5
Matus Chochlik <chochlik@gmail.com>
2016-11-10 06:06:26 +0100

    Updated the quickbook docs

---
+++ doc/quickbook/refltool/example/to_rapidjson.cpp
@@ -0,0 +1,104 @@
+/*
+ * Copyright Matus Chochlik.
+ * Distributed under the Boost Software License, Version 1.0.
+ * See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt
+ */
+//[refltool_example_to_rapidjson_1
+#include <refltool/to_rapidjson.hpp>
+#include <rapidjson/ostreamwrapper.h>
+#include <rapidjson/prettywriter.h>
+#include <iostream>
+//]
+//[refltool_example_to_rapidjson_2
+enum gender
+{
+	female, male
+};
+
+enum marital_status
+{
+	single, married, divorced, widowed, unknown
+};
+
+struct person
+{
+	std::string first_name;
+	std::string middle_name;
+	std::string surname;
+
+	gender sex;
+	marital_status status;
+
+	unsigned age;
+
+	bool drivers_license;
+};
+//]
+//[refltool_example_to_rapidjson_3
+int main(void)
+{
+	using namespace refltool;
+
+	// create a list of persons
+	std::vector<person> pers;
+	pers.push_back({"John", "X", "Doe", male, unknown, 40, false});
+	pers.push_back({"Jane", "Y", "Roe", female, unknown, 28, true});
+	pers.push_back({"Johnny", "B", "Goode", male, single, 19, false});
+	pers.push_back({"James", "T", "Kirk", male, divorced, 60, true});
+
+
+	// compose rapidjson document
+	rapidjson::Document doc;
+	to_rapidjson(doc, pers);
+
+	// write to stdout
+	rapidjson::OStreamWrapper osw(std::cout);
+	rapidjson::PrettyWriter<rapidjson::OStreamWrapper> writer(osw);
+	doc.Accept(writer);
+
+	std::cout << std::endl;
+
+	return 0;
+}
+//]
+//[refltool_example_to_rapidjson_output
+[
+    {
+        "first_name": "John",
+        "middle_name": "X",
+        "surname": "Doe",
+        "sex": "male",
+        "status": "unknown",
+        "age": 40,
+        "drivers_license": false
+    },
+    {
+        "first_name": "Jane",
+        "middle_name": "Y",
+        "surname": "Roe",
+        "sex": "female",
+        "status": "unknown",
+        "age": 28,
+        "drivers_license": true
+    },
+    {
+        "first_name": "Johnny",
+        "middle_name": "B",
+        "surname": "Goode",
+        "sex": "male",
+        "status": "single",
+        "age": 19,
+        "drivers_license": false
+    },
+    {
+        "first_name": "James",
+        "middle_name": "T",
+        "surname": "Kirk",
+        "sex": "male",
+        "status": "divorced",
+        "age": 60,
+        "drivers_license": true
+    }
+]
+//]
---
+++ doc/quickbook/refltool/quickref/to_rapidjson.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright Matus Chochlik.
+ * Distributed under the Boost Software License, Version 1.0.
+ * See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt
+ */
+//[refltool_to_rapidjson_value
+// to_rapidjson
+template <typename Encoding, typename Allocator, typename T>
+static inline
+__rapidjson_GenericValue<Encoding, Allocator>& to_rapidjson(
+	__rapidjson_GenericValue<Encoding, Allocator>& rjval,
+	Allocator& alloc,
+	const T& val
+);
+//]
+//[refltool_to_rapidjson_doc
+template <typename Encoding, typename T>
+static inline
+__rapidjson_GenericDocument<Encoding>& to_rapidjson(
+	__rapidjson_GenericDocument<Encoding>& rjdoc,
+	const T& val
+);
+//]
+//[refltool_rapidjson_compositor
+template <typename T>
+struct rapidjson_compositor
+{
+	template <typename Encoding, typename Allocator>
+	void operator()(
+		__rapidjson_GenericValue<Encoding, Allocator>& rjval,
+		Allocator& alloc,
+		const T& v
+	) const;
+};
+//]
+
