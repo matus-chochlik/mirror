524f85437485399f7c5f3c45eea2c64717788a92
Matus Chochlik <chochlik@gmail.com>
2021-03-18 21:55:25 +0100

    Initial build system setup

---
+++ config/Defaults.cmake
@@ -0,0 +1,7 @@
+#  Copyright Matus Chochlik.
+#  Distributed under the Boost Software License, Version 1.0.
+#  See accompanying file LICENSE_1_0.txt or copy at
+#   http://www.boost.org/LICENSE_1_0.txt
+#
+set(MIRROR_LLVM_PREFIX /opt/llvm)
+set(CMAKE_CXX_COMPILER ${MIRROR_LLVM_PREFIX}/bin/clang++)
---
+++ include/mirror/meta.hpp
@@ -0,0 +1,24 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+#ifndef MIRROR_META_HPP
+#define MIRROR_META_HPP
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wunused-member-function"
+#pragma clang diagnostic ignored "-Wshadow-uncaptured-local"
+
+#include <experimental/compiler>
+#include <experimental/meta>
+
+#pragma clang diagnostic pop
+
+#endif
+
