25c85b3b3c3f9c18d26997dbcee80b6193b73d61
Matus Chochlik <chochlik@gmail.com>
2021-12-11 10:09:34 +0100

    Added new example

--- example/next_weekday.cpp
+++ example/next_weekday.cpp
@@ -1,3 +1,10 @@
+/// @example next_weekday
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
 #include <mirror/enum_utils.hpp>
 #include <iostream>
 
