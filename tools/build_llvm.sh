dacdc78aa351bdf7b06730040f6a5f8c0906964f
Matus Chochlik <chochlik@gmail.com>
2021-12-12 12:55:01 +0100

    Small changes in the rebuild_llvm script

--- tools/build_llvm.sh
+++ tools/build_llvm.sh
@@ -6,24 +6,26 @@
 #
 tool_dir=$(realpath "$(dirname ${0})")
 root_dir=$(dirname "${tool_dir}")
-llvm_install_prefix=/opt/mirror/llvm
+llvm_install_dir=/opt/mirror/llvm
 llvm_build_dir="${tool_dir}/_build"
 compile_jobs=$(grep -c -e 'processor\s\+:\s\+[0-9]\+' < /proc/cpuinfo)
 link_jobs=$(grep MemTotal /proc/meminfo | awk '{printf "%.0f",  $2/(1024*1024*6)}')
 linker=lld
+clean="false"
 
 if [[ -f "${root_dir}/LLVM_PREFIX" ]]
-then llvm_install_prefix=$(< "${root_dir}/LLVM_PREFIX")
+then llvm_install_dir=$(< "${root_dir}/LLVM_PREFIX")
 fi
 
-while getopts "b:C:L:l:" arg
+while getopts "b:C:L:l:c" arg
 do
 	case ${arg} in
 		b) llvm_build_dir=${OPTARG};;
-		i) llvm_install_prefix=${OPTARG};;
+		i) llvm_install_dir=${OPTARG};;
 		C) compile_jobs=${OPTARG};;
 		L) link_jobs=${OPTARG};;
 		l) linker=${OPTARG};;
+		c) clean="true";;
 		*) exit 1;;
 	esac
 done
@@ -32,13 +34,16 @@ if [[ "x${llvm_build_dir}" == "x" ]]
 then echo "invalid build directory path '${llvm_build_dir}'"; exit 2
 fi
 
+if [[ "${clean}" == "true" ]]
+then rm -rf "${llvm_build_dir}/*"
+fi
+
 mkdir -p "${llvm_build_dir}" && \
-rm -rf "${llvm_build_dir}/*" && \
 pushd "${llvm_build_dir}" && \
 cmake \
 	-G Ninja \
 	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_INSTALL_PREFIX="${llvm_install_prefix}" \
+	-DCMAKE_INSTALL_PREFIX="${llvm_install_dir}" \
 	-DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=${linker}" \
 	-DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=${linker}" \
 	-DLLVM_ENABLE_PROJECTS="clang" \
