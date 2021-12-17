e2ff9a9d1cea929f84874913019cdfba79e3d058
Matus Chochlik <chochlik@gmail.com>
2021-12-17 08:49:40 +0100

    Doxygen setup

---
+++ doc/doxygen/include/eagine.hpp
@@ -0,0 +1,20 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+/// @mainpage The Mirror library reference
+///
+/// This is the Doxygen-generated reference documentation for the Mirror project.
+///
+/// @note This documentation is work-in-progress and incomplete.
+
+/// @brief Mirror's code is placed in this namespace.
+namespace mirror {
+
+/// @defgroup classification Metaobject classification
+
+} // namespace mirror
