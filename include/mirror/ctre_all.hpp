5bc810dfdaa21e1597132839450c626a65968797
Matus Chochlik <chochlik@gmail.com>
2022-01-08 01:58:53 +0100

    Added the CTRE sub-module and a header including it

---
+++ include/mirror/ctre_all.hpp
@@ -0,0 +1,23 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+#ifndef MIRROR_CTRE_ALL_HPP
+#define MIRROR_CTRE_ALL_HPP
+
+#include "diagnostic.hpp"
+
+MIRROR_DIAG_PUSH()
+#if defined(__clang__)
+MIRROR_DIAG_OFF(reserved-macro-identifier)
+MIRROR_DIAG_OFF(newline-eof)
+MIRROR_DIAG_OFF(ctad-maybe-unsupported)
+#endif
+#include <ctre.hpp>
+MIRROR_DIAG_POP()
+
+#endif
