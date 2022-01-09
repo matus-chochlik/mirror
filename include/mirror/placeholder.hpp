df2b4f3afa0a49ea95334c5791fad5367f132820
Matus Chochlik <chochlik@gmail.com>
2022-01-09 08:14:18 +0100

    Added new placeholder expression functions

--- include/mirror/placeholder.hpp
+++ include/mirror/placeholder.hpp
@@ -624,6 +624,13 @@ consteval auto get_base_type(placeholder_expr<X> e) {
 }
 
 template <typename X>
+consteval auto get_element_type(placeholder_expr<X> e) {
+    return placeholder_expr{[e](auto mo) {
+        return get_element_type(e(mo));
+    }};
+}
+
+template <typename X>
 consteval auto get_underlying_type(placeholder_expr<X> e) {
     return placeholder_expr{[e](auto mo) {
         return get_underlying_type(e(mo));
@@ -837,10 +844,53 @@ consteval auto make_sequence(placeholder_expr<X>... e) {
     }};
 }
 
+template <typename... X>
+consteval auto concat(placeholder_expr<X>... e) {
+    return placeholder_expr{[e...](auto mo) {
+        return concat(e(mo)...);
+    }};
+}
+
+template <typename X, typename F>
+consteval auto all_of(placeholder_expr<X> e, F predicate) {
+    return placeholder_expr{[e, predicate](auto mo) {
+        return all_of(e(mo), predicate);
+    }};
+}
+
+template <typename X, typename F>
+consteval auto any_of(placeholder_expr<X> e, F predicate) {
+    return placeholder_expr{[e, predicate](auto mo) {
+        return any_of(e(mo), predicate);
+    }};
+}
+
+template <typename X, typename F>
+consteval auto none_of(placeholder_expr<X> e, F predicate) {
+    return placeholder_expr{[e, predicate](auto mo) {
+        return none_of(e(mo), predicate);
+    }};
+}
+
 template <typename X, typename F>
 consteval auto filter(placeholder_expr<X> e, F predicate) {
     return placeholder_expr{[e, predicate](auto mo) {
         return filter(e(mo), predicate);
+    }};
+}
+
+template <typename X, typename Function, typename Fallback, typename... P>
+consteval auto select(
+  placeholder_expr<X> e,
+  Function function,
+  Fallback fallback,
+  P&&... param) {
+    return placeholder_expr{[e, &function, &fallback, &param...](auto mo) {
+        return select(
+          e(mo),
+          std::move(function),
+          std::move(fallback),
+          std::forward<P>(param)...);
     }};
 }
 
