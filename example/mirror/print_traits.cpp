12ee8a435d593a3e006948db423e1e505e51234c
Matus Chochlik <chochlik@gmail.com>
2022-01-17 07:48:03 +0100

    Updated example (print_traits)

--- example/mirror/print_traits.cpp
+++ example/mirror/print_traits.cpp
@@ -16,8 +16,7 @@
 
 void print_traits(mirror::metaobject auto mo) {
     const auto mes = get_enumerators(mirror(mirror::meta_trait));
-    const auto maxl = fold_init_list(
-      mes, get_size(get_name(mirror::_1)), [](auto l) { return std::max(l); });
+    const auto maxl = get_top_value(mes, get_size(get_name(mirror::_1)));
 
     std::cout << "traits of " << get_display_name(mo) << std::endl;
     for_each(mes, [&](mirror::metaobject auto me) {
