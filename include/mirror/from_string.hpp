e90fa7d5949a6bb2b4dfef267d0c43e4ff8906f0
Matus Chochlik <chochlik@gmail.com>
2022-01-18 07:26:28 +0100

    Fixes in from_string

--- include/mirror/from_string.hpp
+++ include/mirror/from_string.hpp
@@ -198,11 +198,12 @@ auto from_string(const std::string_view src) noexcept {
 }
 
 template <typename T>
-auto from_optional_string(const std::optional<std::string_view> src) noexcept
-  -> std::optional<T> {
+auto from_optional_string(
+  const std::optional<std::string_view> src,
+  std::type_identity<T> tid = {}) noexcept -> std::optional<T> {
     if(src) {
-        if(const auto converted{from_string(src, std::type_identity<T>{})}) {
-            return extract(converted);
+        if(auto converted{from_string(*src, tid)}; has_value(converted)) {
+            return {std::move(extract(converted))};
         }
     }
     return {};
